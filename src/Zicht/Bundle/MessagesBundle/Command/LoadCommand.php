63d62e30c8c59bf71c2e3f7616de1db8e914e78f
Gerard van Helden <gerard@zicht.nl>
2014-10-01 09:08:25 +0000

    now supporting yml files for loading as well
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtMessagesBundle/trunk@46403 0f35748b-8228-47f0-bbea-889afbbd9a42

--- src/Zicht/Bundle/MessagesBundle/Command/LoadCommand.php
+++ src/Zicht/Bundle/MessagesBundle/Command/LoadCommand.php
@@ -8,6 +8,11 @@ namespace Zicht\Bundle\MessagesBundle\Command;
 
 use \Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 
+use Symfony\Component\Translation\Loader\PhpFileLoader;
+use Symfony\Component\Translation\Loader\YamlFileLoader;
+use Symfony\Component\Translation\MessageCatalogueInterface;
+use Symfony\Component\Translation\Translator;
+use Zicht\Bundle\MessagesBundle\Entity\Message;
 use \Zicht\Bundle\MessagesBundle\Entity\MessageTranslation;
 
 use Symfony\Component\Console\Input\InputArgument;
@@ -43,54 +48,78 @@ class LoadCommand extends ContainerAwareCommand {
      * @param \Symfony\Component\Console\Output\OutputInterface $output
      */
     protected function execute(InputInterface $input, OutputInterface $output) {
-        $doctrine = $this->getContainer()->get('doctrine');
-        $em = $doctrine->getManager();
-
         $filename = $input->getArgument('file');
-        $loader = new \Symfony\Component\Translation\Loader\PhpFileLoader();
+
+        $ext = pathinfo($filename, PATHINFO_EXTENSION);
+        $loaders = array(
+            'php' => new PhpFileLoader(),
+            'yml' => new YamlFileLoader()
+        );
+
         $overwrite = $input->getOption('overwrite');
 
-        if(preg_match('/(.*)\.(\w+)\.php/', basename($filename), $m)) {
-            $catalogue = $loader->load($filename, $m[2], $m[1]);
+        if (!array_key_exists($ext, $loaders) || !preg_match('/(.*)\.(\w+)\.[^.]+/', basename($filename), $m)) {
+            $output->writeln('Unsupported file type: ' . $filename);
+            return 1;
+        } else {
+            $catalogue = $loaders[$ext]->load($filename, $m[2], $m[1]);
 
-            $n = 0;
-            foreach ($catalogue->all() as $domain => $messages) {
-                foreach ($messages as $key => $translation) {
-                    try {
-                        $record = new \Zicht\Bundle\MessagesBundle\Entity\Message();
-                        $record->message = $key;
-                        $record->domain = $domain;
-                        $existing = $em->getRepository('\Zicht\Bundle\MessagesBundle\Entity\Message')->findOneBy(array(
-                            'message' => $key,
-                            'domain' => $domain
-                        ));
+            $numLoaded = $this->loadMessages(
+                $catalogue,
+                $overwrite,
+                function ($e, $key) use($output) {
+                    $output->writeln(sprintf("<error>%s</error> while processing message %s\n", $e->getMessage(), $key));
+                }
+            );
 
-                        if ($existing) {
-                            if ($translationEntity = $existing->hasTranslation($catalogue->getLocale())) {
-                                if ($overwrite) {
-                                    $translationEntity->translation = $translation;
-                                } else {
-                                    continue;
-                                }
+            $output->writeln(sprintf("<info>%d</info> messages loaded", $numLoaded));
+        }
+    }
+
+
+    protected function loadMessages(MessageCatalogueInterface $catalogue, $overwrite, $onError)
+    {
+        $em = $this->getContainer()->get('doctrine')->getManager();
+
+        $n = 0;
+        foreach ($catalogue->all() as $domain => $messages) {
+            foreach ($messages as $key => $translation) {
+
+                try {
+                    $record = new Message();
+                    $record->message = $key;
+                    $record->domain = $domain;
+                    /** @var Message $existing */
+                    $existing = $em->getRepository('\Zicht\Bundle\MessagesBundle\Entity\Message')->findOneBy(array(
+                        'message' => $key,
+                        'domain' => $domain
+                    ));
+
+                    if ($existing) {
+                        if ($translationEntity = $existing->hasTranslation($catalogue->getLocale())) {
+                            if ($overwrite) {
+                                $translationEntity->translation = $translation;
                             } else {
-                                $existing->addTranslations(new MessageTranslation($catalogue->getLocale(), $translation));
+                                continue;
                             }
-                            $record = $existing;
                         } else {
-                            $record->addTranslations(new MessageTranslation($catalogue->getLocale(), $translation));
+                            $existing->addTranslations(new MessageTranslation($catalogue->getLocale(), $translation));
                         }
-                        $em->persist($record);
-                        $em->flush();
+                        $record = $existing;
+                    } else {
+                        $record->addTranslations(new MessageTranslation($catalogue->getLocale(), $translation));
+                    }
+                    $em->persist($record);
+                    $em->flush();
 
-                        $n ++;
-                    } catch(\Exception $e) {
-                        $output->writeln(sprintf("<error>%s</error> while processing message %s\n", $e->getMessage(), $key));
+                    $n ++;
+                } catch (\Exception $e) {
+                    if (is_callable($onError)) {
+                        $onError($e, $key);
                     }
                 }
             }
-            $output->writeln(sprintf("<info>%d</info> messages loaded", $n));
-        } else {
-            $output->writeln('Unsupported file type: ' . $filename);
         }
+        return $n;
     }
 }
